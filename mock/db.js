cdc4046b2edc628106c2e1a9d6aa7bc6714fe5d2
baishusama <baishusama@outlook.com>
2018-09-10 08:10:07 +0800

    reuse `generateMember` function in db.js

--- mock/db.js
+++ mock/db.js
@@ -2,18 +2,15 @@ const faker = require('faker');
 const _ = require('lodash');
 const { NUMBER_OF_HERO, NUMBER_OF_VILLAIN } = require('./counts');
 
-const heroes = _.times(NUMBER_OF_HERO, n => ({
+const generateMember = n => ({
   id: n,
   name: faker.fake('{{name.lastName}}, {{name.firstName}} {{name.suffix}}'), // faker.name.findName(),
   email: faker.internet.email(),
   avatar: faker.internet.avatar()
-}));
-const villains = _.times(NUMBER_OF_VILLAIN, n => ({
-  id: n,
-  name: faker.fake('{{name.lastName}}, {{name.firstName}} {{name.suffix}}'), // faker.name.findName(),
-  email: faker.internet.email(),
-  avatar: faker.internet.avatar()
-}));
+});
+
+const heroes = _.times(NUMBER_OF_HERO, generateMember);
+const villains = _.times(NUMBER_OF_VILLAIN, generateMember);
 
 module.exports = (req, res, next) => ({
   heroes,
